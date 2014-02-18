5d5bec7508be559ecbd3e0f66b794387140c233d
Eduardo Sorribas <eduardo@sorribas.org>
2014-02-18 15:34:09 +0100

    Add functionality to fetch team members with basic http auth.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var redirect = function(url, response) {
 };
 
 module.exports = function(clientId, clientSecret, config) {
-	var scope = (config.team)  ? 'user' : 'public';
+	var scope = (config.team && !config.credentials)  ? 'user' : 'public';
 	var secret = config.secret || 'asdasd123123';
 	var userAgent = config.ua || 'github-auth';
 
@@ -36,29 +36,66 @@ module.exports = function(clientId, clientSecret, config) {
 		});
 	};
 
-	var isInTeam = function(accessToken, callback) {
-		if (!config.organization) return callback(new Error('The organization is required to validate the team.'));
-		if (!config.team) return callback(new Error('The team is required.'));
-		request('https://api.github.com/user/teams?access_token=' + accessToken, {
+	var teamId;
+	var getTeamId = function(cb) {
+		request('https://api.github.com/orgs/'+ config.organization + '/teams',{
 			headers: {
 				'User-Agent': userAgent
+			},
+			auth: {
+				user: config.credentials.user,
+				pass: config.credentials.pass
 			}
 		}, function(err, res, body) {
-			if (err) return callback(err);
-
-			var json;
-			try { json = JSON.parse(body); }
+			if (err) return cb(err);
+			if (res.statusCode >= 300) return cb(new Error('Bad credentials'));
+			var teams;
+			try { teams = JSON.parse(body); }
 			catch(e) { 
-				return callback(new Error(body), null);
+				return cb(new Error(body), null);
 			}
 
-			if (!Array.isArray(json)) return callback(null, false);
-			var teamNames = json.map(function(obj) { return obj.slug; });
-			var orgLogins = json.map(function(obj) { return obj.organization.login; });
-			var authorized = teamNames.indexOf(config.team) !== -1 && orgLogins.indexOf(config.organization) !== -1;
-
-			callback(null, authorized);
+			console.log(teams);
+			var teamId = teams.filter(function(x) {return x.name === config.team;})[0].id;
+			cb(null, teamId);
 		});
+	};
+
+	var isInTeam = function(accessToken, ghusr, callback) {
+		if (!config.organization) return callback(new Error('The organization is required to validate the team.'));
+		if (!config.team) return callback(new Error('The team is required.'));
+
+		if (config.credentials) {
+			getTeamId(function(err, tid) {
+				if (err) return callback(err);
+				getUsersOnTeam(tid, function(err, users) {
+					if (err) return callback(err);
+					callback(null, users.indexOf(ghusr) !== -1);
+				});
+			});
+		} else {
+			request('https://api.github.com/user/teams?access_token=' + accessToken, {
+				headers: {
+					'User-Agent': userAgent
+				}
+			}, function(err, res, body) {
+				if (err) return callback(err);
+
+				var json;
+				try { json = JSON.parse(body); }
+				catch(e) { 
+					return callback(new Error(body), null);
+				}
+
+				if (!Array.isArray(json)) return callback(null, false);
+				var teamNames = json.map(function(obj) { return obj.slug; });
+				var orgLogins = json.map(function(obj) { return obj.organization.login; });
+				var authorized = teamNames.indexOf(config.team) !== -1 && orgLogins.indexOf(config.organization) !== -1;
+
+				callback(null, authorized);
+			});
+		}
+		
 	};
 
 	var isInOrganization = function(accessToken, callback) {
@@ -83,6 +120,31 @@ module.exports = function(clientId, clientSecret, config) {
 		});
 	};
 
+	var lastGhUpdate = 0;
+	var authUsers = [];
+	var tenMinutes = 1000*60*10;
+
+	var getUsersOnTeam = function(teamId, cb) {
+		if ((new Date().getTime() - lastGhUpdate) < tenMinutes) return cb(null, authUsers);
+		var opts = {
+			url: 'https://api.github.com/teams/'+teamId+'/members', 
+			headers: {
+				'User-Agent': userAgent 
+			},
+			auth: {
+				'user': config.credentials.user,
+				'pass': config.credentials.pass
+			}
+		};
+		lastGhUpdate = new Date().getTime();
+		request.get(opts, function(err, res, body) {
+			if (err) return cb(err);
+			if (res.statusCode >= 300) return cb(new Error('Bad credentials'));
+			var usrsObj = JSON.parse(body);
+			authUsers = usrsObj.map(function(x) { return x.login; });
+			cb(null, authUsers);
+		});
+	};
 
 	return function(req, res, next) {
 		var cookie = getCookie(req, cookieName);
@@ -100,6 +162,7 @@ module.exports = function(clientId, clientSecret, config) {
 					code: u.query.code
 				}
 			}, function(err, response, body) {
+				if (err) return next(err);
 				var resp = url.parse('/?'+body, true);
 				var accessToken = resp.query.access_token;
 
@@ -108,7 +171,7 @@ module.exports = function(clientId, clientSecret, config) {
 
 					var checks = [];
 					if (config.users) checks.push(function(cb) { cb(null, config.users.indexOf(ghusr) !== -1); });
-					if (config.team) checks.push(function(cb) { isInTeam(accessToken, cb); });
+					if (config.team) checks.push(function(cb) { isInTeam(accessToken, ghusr, cb); });
 					if (config.organization) checks.push(function(cb) { isInOrganization(accessToken, cb); });
 
 					async.parallel(checks, function(err, results) {
